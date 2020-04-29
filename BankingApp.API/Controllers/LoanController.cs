816a091ec2472a04d139c3631bd56d07b39e4b79
Adrian <cpaadrian2@gmail.com>
2020-04-29 22:10:14 +0300

    Added LoanRequest Evaluation

--- BankingApp.API/Controllers/LoanController.cs
+++ BankingApp.API/Controllers/LoanController.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
+using BankingApp.API.Helpers;
 using BankingApp.API.Models.Loans;
 using BankingApp.API.Repositories.Interfaces;
 using BankingApp.Domain.Entities;
@@ -15,9 +17,9 @@ namespace BankingApp.API.Controllers {
     public class LoanController: ControllerBase {
         private readonly IRepository _repo;
         private readonly IMapper _mapper;
-        private readonly UserManager<Customer> _customerManager;
+        private readonly UserManager<User> _customerManager;
 
-        public LoanController(IRepository repo, IMapper map, UserManager<Customer> custom) {
+        public LoanController(IRepository repo, IMapper map, UserManager<User> custom) {
             _repo = repo;
             _mapper = map;
             _customerManager = custom;
@@ -25,7 +27,10 @@ namespace BankingApp.API.Controllers {
 
         [HttpPostAttribute("request/")]
         public async Task<IActionResult> RequestLoan([FromBody]LoanRequestModel model) {
-            model.CustomerId = Int32.Parse(User.Identity.Name);
+            var userId = Int32.Parse(User.Identity.Name);
+            var customers = await _repo.GetWithWhere<Customer>(o => o.UserId == userId);
+            var customer = customers.FirstOrDefault();
+            model.CustomerId = customer.Id;
             var loanRequest = _mapper.Map<LoanRequest>(model);
             loanRequest.Status = LoanRequestStatus.Pending;
             await _repo.Add(loanRequest);
@@ -36,17 +41,37 @@ namespace BankingApp.API.Controllers {
         [HttpPostAttribute("reject/{id}")]
         public async Task<IActionResult> DeclineLoanRequest(int id) {
             var loanRequest = await _repo.GetById<LoanRequest>(id);
+
+            if (loanRequest.Status != LoanRequestStatus.Pending) {
+                throw new AppException("Bank Account already evaluated");
+            }
+
+            loanRequest.Status = LoanRequestStatus.Declined;
+            var requestAction = await EvaluateLoanRequest(id, ActionType.Reject);
+            await _repo.Add(requestAction);
+            return Ok(requestAction);
+        }
+
+        [HttpPostAttribute("acceot/{id}")]
+        public async Task<IActionResult> AcceptLoanRequest(int id) {
+            var loanRequest = await _repo.GetById<LoanRequest>(id);
+
+            if (loanRequest.Status != LoanRequestStatus.Pending) {
+                throw new AppException("Bank Account already evaluated");
+            }
+             
             loanRequest.Status = LoanRequestStatus.Accepted;
             var requestAction = await EvaluateLoanRequest(id, ActionType.Accept);
             await _repo.Add(requestAction);
             return Ok(requestAction);
         }
 
-
         public async Task<LoanRequestAction> EvaluateLoanRequest(int id, ActionType action) {
-            var officerId = await Task.FromResult<int>(Int32.Parse(User.Identity.Name));
+            var userId = await Task.FromResult<int>(Int32.Parse(User.Identity.Name));
+            var officers = await _repo.GetWithWhere<LoanOfficer>(o => o.UserId == userId);
+            var officer = officers.FirstOrDefault();
             var model = new EvaluateLoanRequestModel() {
-                LoanOfficerId = officerId,
+                LoanOfficerId = officer.Id,
                 LoanRequestId = id,
                 Action = action
             };
